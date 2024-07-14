534c42e122a4e57ebe4b7b10c0c7d6c977dbbe8d
NahidRuhan <dilal328@gmail.com>
2024-07-14 22:45:46 +0600

    added basename setting to see if /civil redirect works fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import React from 'eact'
-import ReactDOM from 'eact-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import {
   createBrowserRouter,
   RouterProvider,
